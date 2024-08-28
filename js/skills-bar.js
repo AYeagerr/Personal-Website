46cae6de2f407da2dbf5ef42a962f7c19e6a2523
Abdulrhman Mohamed <138893669+AYeagerr@users.noreply.github.com>
2024-08-28 03:44:55 +0300

    Add files via upload

---
+++ js/skills-bar.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+    document.querySelector('.skill-html').style.width = '100%';
+    document.querySelector('.skill-css').style.width = '75%';
+    document.querySelector('.skill-js').style.width = '50%';
+});
